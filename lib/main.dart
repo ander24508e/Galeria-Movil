610cd1815cb2a2cf11d855fcebfafb4dfa749f4a
Ander <adeb2737@gmail.com>
2024-06-26 22:57:31 -0500

    Segundo commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hola Mundo',
+      title: 'GALERIA',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'GALERIA DE TURISMO'),
     );
   }
 }
@@ -24,50 +23,54 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
-}
+}          
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: ListView(
+        children: <Widget>[
+          const SizedBox(height: 15),
+          Container(
+            padding: const EdgeInsets.all(20.0),
+            child: Image.asset('lib/imagenes/imagen-turismo.jpeg'),
+          ),
+          const SizedBox(height: 15),
+          Container(
+            padding: const EdgeInsets.all(20.0),
+            child: Image.network('https://www.upsjb.edu.pe/wp-content/uploads/2024/03/40-turismo--1024x592.jpeg')
+          ),
+          const SizedBox(height: 15),
+          Container(
+            padding: const EdgeInsets.all(20.0),
+            child: Image.network('https://cdn.diferenciador.com/imagenes/turismo-aventura-cke.jpg')
+          ),
+          const SizedBox(height: 15),
+          Container(
+            padding: const EdgeInsets.all(20.0),
+            child: Image.asset('lib/imagenes/imagen-turismo2.jpg'),
+          ),
+          const SizedBox(height: 15),
+          Container(
+            padding: const EdgeInsets.all(20.0),
+            child: Image.asset('lib/imagenes/imagen-turismo3.jpg'),
+          ),
+          const SizedBox(height: 15),
+          Container(
+            padding: const EdgeInsets.all(20.0),
+            child: Image.network('https://mytrip2ecuador.com/wp-content/uploads/2022/03/ecuador-mainland.jpg')
+          ),
+          const SizedBox(height: 15),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
